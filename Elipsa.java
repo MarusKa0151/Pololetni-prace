12fbf7edb1c861fa45ee8ac29943186109f807cc
Mája <m.skola>
2018-11-28 20:48:10 +0100

    Why this does not work?

--- Elipsa.java
+++ Elipsa.java
@@ -1,3 +1,27 @@
-public static Elipsa {
-    
+public class Elipsa { //do not work, why?
+    public static void main(String[] args) {
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        draw(a, b);
+    }
+
+    public static void draw(double a, double b) {
+        for (double x = -a; x <= a; x++) {
+
+            for (double y = b; y >= -b; y--) {
+
+                if (inEllipse(x, y, a, b)) {
+                    System.out.print("X");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+        }
+    }
+        
+    //checking if point is in the ellipse
+    public static boolean inEllipse(double x, double y, double a, double b) {
+        return (x * x + y * y) <= (((x / a)*(x / a)) + ((y / b)*(y / b)));
+    }
 }
