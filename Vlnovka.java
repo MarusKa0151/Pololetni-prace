7d49787f542e6857a91ac2bea5f9ae39bb2ae617
Mája <m.skola>
2018-12-06 15:06:08 +0100

    Almost done.

--- Vlnovka.java
+++ Vlnovka.java
@@ -1,50 +1,66 @@
 public class Vlnovka {
+    //TBD
     public static void main(String[] args) {
-        int size = Int.parseInt(args[0]);
+        int sizeOfWave = Int.parseInt(args[0]);
         int widthOfScreen = 80;
         int upperSpaces = (size * 2) - 3;
         int lowerSpaces = 1;
 
+        /*first line*/
+        if (size != 0) {
+            boolean printingLowerSpaces = false;
+            boolean printingUpperSpaces = false;
+            boolean charDown = true;
+            boolean charUp = false;
+
+            
+        }
+
+
         /*printing lines*/
-        for (int i = 0; i < size; i++) {
-            j = 0;
-            if (i == 0) {
-                boolean printingLowerSpaces = false;
-                boolean printingUpperSpaces = false;
-                boolean charU = true;
-                boolean charL = false;
-            }
-            else {
-                boolean printingLowerSpaces = true;
-                boolean printingUpperSpaces = false;
-                boolean charL = false;
-                boolean charU = false;
-            }
-            /*printing chars*/ //NEFUNGUJE TI TA TEORIE, OPRAV TO!!!
-            while (j < widthOfScreen) {
+    for (int i = 1; i < sizeOfWave; i++) {  
+        int j = 0;
+        while (j < widthOfScreen) {
 
-                if (printingLowerSpaces == true) {
-                    for (int l = 0; l < lowerSpaces; l++) {
+            if (charDown = true && j < widthOfScreen) {
+                System.out.print("#");
+                charDown = false;
+                printingUpperSpaces = true;
+                j++;
+            }
+
+            if (printingUpperSpaces = true) {
+                for (int u = 0; u < upperSpaces; u++) {
+                    if (j < widthOfScreen) {
                         System.out.print(" ");
-                        lowerSpaces += 2;
+                        j++;
                     }
-                    printingLowerSpaces = false;
-                    charU = true;
                 }
-
-                else if (printingUpperSpaces == true) {
-                    for (int u = 0; u < upperSpaces; u++) {
-                        System.out.print(" ");
-                        upperSpaces -= 2;
-                    }
-                    printingUpperSpaces = false;
-                    charL = true;
-                }
-                
-
-
+                printingUpperSpaces = false;
+                charUp = true;
+                upperSpaces -= 2;
             }
 
+            if (charUp = true && j < widthOfScreen) {
+                System.out.print("#");
+                charUp = false;
+                printingLowerSpaces = true;
+                j++;
+            }
+
+            if (printingLowerSpaces = true) {
+                for (int l = 0; l < lowerSpaces; l++) {
+                    if (j < widthOfScreen) {
+                        System.out.print(" ");
+                        j++;
+                    } 
+                }
+                printingLowerSpaces = false;
+                charDown = true;
+                lowerSpaces += 2;
+            }
+            
+        }
             System.out.println();
         }
         
