370cc0f4f0f4878ba7abc613cebba1864271518a
Marie <m.skola>
2019-01-04 15:38:04 +0100

    CetnostCisel works for all integers

--- CetnostCisel.java
+++ CetnostCisel.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*Returning numbers from the most times written, in paranthesis is number of repetition */
+/*Returning integers from the most times written, in paranthesis is number of repetition */
 public class CetnostCisel {  
     public static void main(String[] args) {
 
@@ -15,42 +15,63 @@ public class CetnostCisel {
                 maximum = sc.nextInt();
                 numbersFromScanner.add(0, maximum);
             }
-            
             position += 1;
             numbersFromScanner.add(position, sc.nextInt());
-            if (numbersFromScanner.get(position) > maximum) {
-                maximum = numbersFromScanner.get(position);
+
+            if (numbersFromScanner.get(position) > maximum || numbersFromScanner.get(position) < -maximum) {
+                maximum = -1 * numbersFromScanner.get(position);
             }
         }
         //System.out.printf("Maximum is: %d \n", maximum);
 
         /*creating Array full of 0 for following work with indexes*/
         int[] arrayOfNumbers = new int[maximum + 1];
+        int[] arrayOfNegativeNumbers = new int[maximum + 1];
         for (int j = 0; j <= maximum; j++) {
             arrayOfNumbers[j] = 0;
+            arrayOfNegativeNumbers[j] = 0;
         }
 
         /*counting*/
         for (int i = 0; i < numbersFromScanner.size(); i++) {
             int number2 = numbersFromScanner.get(i);
-            arrayOfNumbers[number2] += 1;
+            if (number2 >= 0) {
+                arrayOfNumbers[number2] += 1;
+            }
+            else {
+                arrayOfNegativeNumbers[-number2] += 1;
+            }
         }
         
         /*almost like BubbleSort - haha sure...*/
-        for (int h = 0; h < maximum; h++) {
-            int maximum2 = arrayOfNumbers[0];
-            int maxIndex = 0; //number
-            for (int g = 1; g <= maximum; g++) {
-                if (arrayOfNumbers[g] > maximum2) {
-                    maximum2 = arrayOfNumbers[g];
-                    maxIndex = g;
+        for (int z = 0; z < 2; z++) {
+
+            for (int h = 0; h < maximum; h++) {
+                int maximum2 = arrayOfNumbers[0];
+                int maxIndex = 0; //number
+
+                for (int g = 0; g <= maximum; g++) {
+                    if (arrayOfNumbers[g] > maximum2) {
+                        maximum2 = arrayOfNumbers[g];
+                        maxIndex = g;
+                    }
+                    else if (arrayOfNegativeNumbers[g] > maximum2) {
+                        maximum2 = arrayOfNegativeNumbers[g];
+                        maxIndex = -g;
+                    }
+                }
+                if (maximum2 == 0) { //no more printing needed
+                    break;
+                }
+                
+                System.out.printf("%d(%d) ", maxIndex, maximum2); //number(how many times)
+                if (maxIndex >= 0) {
+                    arrayOfNumbers[maxIndex] = 0;
+                }
+                else {
+                    arrayOfNegativeNumbers[-maxIndex] = 0;
                 }
             }
-            if (maximum2 == 0) { //no more printing needed
-                break;
-            }
-            System.out.printf("%d(%d) ", maxIndex, maximum2); //number(how many times)
-            arrayOfNumbers[maxIndex] = 0;
         }
     }
 }
--- Kalendar.java
+++ Kalendar.java
@@ -1,38 +1,86 @@
+import java.util.Calendar;
+import java.util.Locale;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Kalendar {
     //tbd
-    /*
-    Date date = new Date(); date.toString() -> current date
-    SimpleDateFormat sdf = new SimpleDateFormat(hh:mm:ss a dd-MMM-yyyy); sdf.format(date) -> another date format
-     */
+    
     public static void main(String[] args) {
         if (args.length == 0) {
-
+            calendar(Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.YEAR));
         }
         
         else if (args.length == 1) {
-
+            calendar(Integer.parseInt(args[0]) - 1, Calendar.getInstance().get(Calendar.YEAR));
         }
 
         else if (args.length == 2) {
-            calendar(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        }
-
+            calendar(Integer.parseInt(args[0]) - 1, Integer.parseInt(args[1]));
+        }            
     }
 
     public static void calendar(int month, int year) {
-
-
-
+        String title = months(month) + " " + year;
         System.out.println("+----------------------+");
-        //System.out.println("|%22s|", name);
+        System.out.printf("|%22s|\n", title);
         System.out.println("+----------------------+");
         System.out.println("| Po Ut St Ct Pa So Ne |");
-        /*
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("| %2d %2d %2d %2d %2d %2d %2d |\n");
-        }
-        */
+        //notStaticPart(getDayOfWeek(1, month, year));
         System.out.println("+----------------------+");
 
     }
+
+    public static String months(int month) {
+        String monthsInLetters[] = {"leden", "unor", "brezen", "duben", "kveten", "cerven", "cervenec", "srpen", "zari", "rijen", "listopad", "prosinec"};
+        return monthsInLetters[month]; 
+    }
+
+    public static void notStaticPart(int dayOfWeek) {
+        
+        /*
+        tadyto dodelat!!!!!!!!!!!!!!!!!!!!!111
+        */
+    }
+
+    public static int getDayOfWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        int dayOfWeekIntValue = dayOfWeek.getValue();
+        return dayOfWeekIntValue;
+    }
+
+    public static boolean shorterOrLongerYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        else if (year % 100 == 0) {
+            return false;
+        }
+        else if (year % 4 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static int daysInMonth(int month, int year) {
+        int days[] = {31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (month == 1) {
+            if (shorterOrLongerYear(year)) {
+                return 29;
+            }
+            else {
+                return 28;
+            }
+        }
+        else {
+            return (days[month]);
+        }
+    }
+
+    
+    
 }
